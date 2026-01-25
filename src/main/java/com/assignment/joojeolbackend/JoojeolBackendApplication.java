8eadef77af5596b4dad60f9fdcb57497bbf7ffd3
INnoVatioN-97 <koyoungil97@gmail.com>
2026-01-25 11:34:36 +0900

    init: 프로젝트 생성

---
+++ src/main/java/com/assignment/joojeolbackend/JoojeolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.assignment.joojeolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoojeolBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JoojeolBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/assignment/joojeolbackend/JoojeolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assignment.joojeolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoojeolBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
